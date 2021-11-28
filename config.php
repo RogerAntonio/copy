8d96e7a888c473571beeb468479e661119acabf6
Antonio Vandekerckhove <roger.antonio@live.be>
2021-11-28 16:26:27 +0100

    Version 0.2

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$config = array();
+$config['name'] = "Alerts";
+$config['author'] = "Weblama";
+$config['no_cache'] = false;
+$config['ui'] = true;
+$config['ui_admin'] = false;
+$config['is_system'] = true;
+$config['position'] = 2;
+$config['version'] = 0.2;
+$config['categories'] = "content";
