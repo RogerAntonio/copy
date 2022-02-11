94ccc639aac13d81b3aa0bda95b53eade8b3ef6c
Antonio Vandekerckhove <roger.antonio@live.be>
2022-02-11 07:35:49 +0100

    Fix bootstrapjs from firing multiple times.

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <link rel="stylesheet" href="<?php print $config['url_to_module']; ?>alert.css" />
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+<script>
+  mw.lib.require('bootstrap5js');
+</script>
 <?php
 $alert_id = 'alert-' . $params['id'];
 
